3379cee93c21475edd076cbbfae161417c93d36d
King-BR <fernandopach2017@gmail.com>
2024-09-23 14:30:52 -0300

    Update season 2 info

--- scripts/utils.js
+++ scripts/utils.js
@@ -11,30 +11,3 @@ async function fetchFromLocal(path) {
   }
   return response.json();
 }
-
-function convertArrayToTile(arr) {
-  return {
-    x: arr[0],
-    y: arr[1],
-    data: {
-      alt: arr[2] !== null ? arr[2] : undefined,
-      fertility: arr[3] !== null ? arr[3] : undefined,
-      forest: arr[4] !== null ? arr[4] : undefined,
-      res: arr[5] !== null ? arr[5] : undefined,
-      resAmount: arr[6] !== null ? arr[6] : undefined,
-      region: arr[7] !== null ? arr[7] : undefined,
-      area: arr[8] !== null ? arr[8] : undefined,
-      type: arr[9] !== null ? arr[9] : undefined,
-    },
-  };
-}
-
-async function fetchAndDecompress(path) {
-  const response = await fetch(`./${path}`);
-  const arrayBuffer = await response.arrayBuffer();
-  const decompressed = pako.inflate(new Uint8Array(arrayBuffer), {
-    to: "string",
-  });
-  const parsed = JSON.parse(decompressed);
-  return parsed.map(convertArrayToTile);
-}
