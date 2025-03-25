633a49ef0f257370f7d0a58b7077a023ac4d66e5
King-BR <fernandopach2017@gmail.com>
2025-03-25 20:55:05 -0300

    update heatmaps

--- scripts/heatmap.js
+++ scripts/heatmap.js
@@ -1,5 +1,6 @@
 var townsHouseholds = null;
 var heatmapLayer = null;
+var heatData = [];
 var classes = [
   "nutrition",
   "textile",
@@ -10,80 +11,145 @@ var classes = [
   "mercantile",
 ];
 
-function getHeatmapLayer() {
-  return heatmapLayer;
-}
-
 async function loadHeatmap(type) {
-  map = await getMap();
   townsHouseholds = await getHouseholdData();
-  towns = getTowns();
   heatmapLayer = null;
-  let heatData = [];
+  heatData = [];
+  let max = 0;
 
   switch (type) {
     case "players": {
-      for (let townID in Object.keys(townsHouseholds)) {
+      for (let townID in townsHouseholds) {
         let players = townsHouseholds[townID].filter(
           (h) => h.player && !h.inactive
         );
+        if (players.length > max) max = players.length;
+        let town = towns.find((t) => t.id == townID);
         heatData.push([
-          towns[townID].location.x / 4,
-          mapHeight - towns[townID].location.y / 4,
+          mapHeight - town.location.y / 4,
+          town.location.x / 4,
           players.length,
         ]);
       }
       break;
     }
     case "playersByMainClass": {
-      for (let town in towns) {
-        let players = townsHouseholds[town.id].filter(
+      for (let townID in townsHouseholds) {
+        let players = townsHouseholds[townID].filter(
           (h) => h.player && !h.inactive
         );
+
+        let mainClass = document.getElementById("heatmapClassSelect").value;
+
+        players = players.filter((p) => {
+          let highestSkill = null;
+          Object.keys(p.skills).forEach((skill) => {
+            if (
+              !highestSkill ||
+              parseFloat(p.skills[skill]) > parseFloat(p.skills[highestSkill])
+            ) {
+              highestSkill = skill;
+            }
+          });
+
+          return highestSkill === mainClass;
+        });
+
+        if (players.length > max) max = players.length;
+
+        let town = towns.find((t) => t.id == townID);
+
+        heatData.push([
+          mapHeight - town.location.y / 4,
+          town.location.x / 4,
+          players.length,
+        ]);
       }
       break;
     }
     case "avgPrestige": {
-      for (let town in towns) {
-        let players = townsHouseholds[town.id].filter(
-          (h) => h.player && !h.inactive
-        );
-        let prestige = players.reduce((acc, p) => acc + p.prestige, 0);
+      for (let townID in townsHouseholds) {
+        let households = townsHouseholds[townID];
+        let prestige = households.reduce((a, b) => a + b.prestige, 0);
+        let avgPrestige = prestige / households.length;
+
+        if (avgPrestige > max) max = avgPrestige;
+
+        let town = towns.find((t) => t.id == townID);
+
         heatData.push([
-          town.location.x / 4,
           mapHeight - town.location.y / 4,
-          prestige / players.length,
+          town.location.x / 4,
+          avgPrestige,
         ]);
       }
+      break;
+    }
+    default: {
+      console.error("Invalid heatmap type: " + type);
       break;
     }
   }
 
   heatmapLayer = L.heatLayer(heatData, {
-    radius: 2000,
-    blur: 1,
-    maxZoom: 5,
+    radius: 50,
+    maxZoom: 0,
+    max: max,
+    gradient: {
+      0.0: "blue",
+      0.25: "cyan",
+      0.5: "lime",
+      0.75: "yellow",
+      1.0: "red",
+    },
   });
 }
 
-document.getElementById("toggleHeatmap").addEventListener("change", (event) => {
-  loadHeatmap("players");
+document
+  .getElementById("toggleHeatmap")
+  .addEventListener("change", async (event) => {
+    if (event.target.checked) {
+      await loadHeatmap(document.getElementById("heatmapTypeSelect").value);
+      heatmapLayer.addTo(map);
+      grayscale = true;
+    } else {
+      map.removeLayer(heatmapLayer);
+      grayscale = false;
+    }
 
-  if (event.target.checked) {
-    map.removeLayer(townsLayer);
-    heatmapLayer.addTo(map);
-
-    grayscale = true;
-  } else {
-    map.removeLayer(heatmapLayer);
-    townsLayer.addTo(map);
-
-    grayscale = false;
-  }
-
-  updateTileGrayscale();
-});
+    updateTileGrayscale();
+  });
 
 document
   .getElementById("heatmapTypeSelect")
-  .addEventListener("change", (event) => loadHeatmap(event.target.value));
+  .addEventListener("change", async (event) => {
+    if (document.getElementById("toggleHeatmap").checked) {
+      map.removeLayer(heatmapLayer);
+      heatmapLayer = null;
+
+      if (event.target.value === "playersByMainClass") {
+        document.getElementById("heatmapClassDiv").style.display = "block";
+      } else {
+        document.getElementById("heatmapClassDiv").style.display = "none";
+      }
+
+      await loadHeatmap(event.target.value);
+      heatmapLayer.addTo(map);
+    }
+  });
+
+document
+  .getElementById("heatmapClassSelect")
+  .addEventListener("change", async (event) => {
+    if (
+      document.getElementById("toggleHeatmap").checked &&
+      document.getElementById("heatmapTypeSelect").value ===
+        "playersByMainClass"
+    ) {
+      map.removeLayer(heatmapLayer);
+      heatmapLayer = null;
+
+      await loadHeatmap("playersByMainClass");
+      heatmapLayer.addTo(map);
+    }
+  });
